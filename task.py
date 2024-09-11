c6b4c0dfdcdf3c9794695fa473be465b76173bee
AyeshaShaikSiddiqa <133034872+AyeshaShaikSiddiqa@users.noreply.github.com>
2024-09-11 21:22:21 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,31 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the Ayesha Password Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+letters_part = [random.choice(letters) for _ in range(nr_letters)]
+
+symbols_part = [random.choice(symbols) for _ in range(nr_symbols)]
+
+numbers_part = [random.choice(numbers) for _ in range(nr_numbers)]
+
+
+random.shuffle(letters_part)
+random.shuffle(symbols_part)
+random.shuffle(numbers_part)
+
+
+password_parts = letters_part + symbols_part + numbers_part
+
+
+random.shuffle(password_parts)
+
+
+password = ''.join(password_parts)
+
+print("Your password is: ", password)
